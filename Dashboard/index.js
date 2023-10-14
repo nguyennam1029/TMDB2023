9cf66f7a770a9d7ebd8141774b44f958a2c800e6
Văn Nam Nguyễn <nguyennam1029@gmail.com>
2023-10-14 17:52:29 +0700

    t

--- Dashboard/index.js
+++ Dashboard/index.js
@@ -36,8 +36,42 @@ darkMode.addEventListener('click', () => {
 
 function createProductCard(user) {
 
-// <td class="primary">${formattedDate || user?.timestamp.toDate()} </td>
-console.log(typeof user?.timestamp);
+  const dataProductId = user.role === 'admin' ? '' : `data-product-id="${user.id}"`;
+  const deleteButton = `
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      class="trash delete-button"
+      ${dataProductId}
+    >
+    <path
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+  />
+    </svg>`;
+  
+  const editLink = `
+    <a href="${user.role === 'admin' ? '#' : `/Dashboard/update-user.html?id=${user.id}`}" style="width: 25px; height: 25px; margin: 0;">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="edit"
+      >
+      <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+    />
+      </svg>
+    </a>`;
+
     const template = `
     <tr>
     <td>${user?.displayName}</td>
@@ -48,38 +82,8 @@ console.log(typeof user?.timestamp);
     <td class="primary">${user?.status}</td>
     <td class="primary">${user?.timestamp ? moment.unix(user.timestamp.seconds).format('DD/MM/YYYY') :moment(user?.creationTime, "ddd, DD MMM YYYY HH:mm:ss [GMT]").format("DD/MM/YYYY")}</td>
     <td class="action">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke-width="1.5"
-        stroke="currentColor"
-        class="trash delete-button"
-       
-        data-product-id="${user.id}"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-        />
-      </svg>
-      <a href="${user.role === 'admin' ? 'javascript:alert(\'Không thể xóa\')' : `/Dashboard/update-user.html?id=${user.id}`}" style="width: 25px; height: 25px; margin: 0;">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          class="edit"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
-          />
-        </svg>
-      </a>
+    ${deleteButton}
+    ${editLink}
     </td>
   </tr>
     `;
@@ -113,6 +117,13 @@ const deleteButtons = document.querySelectorAll(".delete-button");
 deleteButtons.forEach((button) => {
   button.addEventListener("click", async () => {
     const id = button.dataset.productId;
+    if(!id) {
+      toastr.options = { 
+        "positionClass": "toast-bottom-left",
+      }
+      toastr.warning('you have no rights');
+      return;
+    }
     try {
       toastr.options = { 
         "positionClass": "toast-bottom-left",
